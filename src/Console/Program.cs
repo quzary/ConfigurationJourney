fd4a48912a9ce4b2a52a336845388eeea1d144fd
agutierrezcu <agutierrezcu@gmail.com>
2017-11-22 17:49:34 +0100

    Library coupled to ConfigurationManager and trust on the application will have a RabbitConnectionString key in its appSetting config

---
+++ src/Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Messaging;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rabbitMqPublisherFactory = new RabbitMqPublisherFactory();
+            rabbitMqPublisherFactory.Create();
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ src/Messaging/RabbitMQPublisherFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Configuration;
+
+namespace Messaging
+{
+    public interface IBusPublisherFactory
+    {
+        object Create();
+    }
+
+    public class RabbitMqPublisherFactory : IBusPublisherFactory
+    {
+        public object Create()
+        {
+            Console.WriteLine("Creating a bus publisher for RabbitMQ");
+
+            var connectionString = ConfigurationManager.AppSettings["RabbitConnectionString"];
+            Console.WriteLine($"Using connection string: {connectionString}");
+            
+            return null;
+        }
+    }
+}
